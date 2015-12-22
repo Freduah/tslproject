7f625e0bf8e5e314420f119688f8d634538f1a36
Administrator <Administrator@FAMPONSAH.tsl.local>
2015-12-22 16:07:29 +0000

    Commit based on recent changes

--- nav/safety_manual_check_nav.php
+++ nav/safety_manual_check_nav.php
@@ -2,7 +2,16 @@
 <nav id="main" class="constrain clearfix">
 <div class="menu-top-container">
     <ul id="menu-top" class="menu">
-        <label>Serial Number : </label> <input type="text" id="truk_serial_number" />   
+        <table>
+            <tr>
+                <td><label style="color: white; font-weight: bold;">SERIAL NO :  </label></td>
+                <td><input type="text" id="truk_serial_number" class="styled-search-textbox" /></td>
+                <td><input type="button" id="search_truck_serial_number" class="styled-search-button" value="SEARCH" /></td>
+                <td><label style="color: white; font-weight: bold;">TRUCK NO : </label></td>
+                <td><input type="text" id="truk_number" class="styled-search-textbox" /></td>
+                <td><input type="button" id="search_truck_number" class="styled-search-button" value="SEARCH" /></td>
+            </tr>
+        </table>
     </ul>
 </div>
 </nav>
