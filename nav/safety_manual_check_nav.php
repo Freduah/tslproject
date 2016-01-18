e017b6a9028283f9e3acaa1315aaba497da0abd1
Administrator <Administrator@FAMPONSAH.tsl.local>
2016-01-18 10:13:06 +0000

    Commit based on recent changes

--- nav/safety_manual_check_nav.php
+++ nav/safety_manual_check_nav.php
@@ -6,12 +6,32 @@
             <tr>
                 <td><label style="color: white; font-weight: bold;">SERIAL NO :  </label></td>
                 <td><input type="text" id="truk_serial_number" class="styled-search-textbox" /></td>
-                <td><input type="button" id="search_truck_serial_number" class="styled-search-button" value="SEARCH" /></td>
                 <td><label style="color: white; font-weight: bold;">TRUCK NO : </label></td>
                 <td><input type="text" id="truk_number" class="styled-search-textbox" /></td>
-                <td><input type="button" id="search_truck_number" class="styled-search-button" value="SEARCH" /></td>
             </tr>
         </table>
     </ul>
 </div>
 </nav>
+
+
+<script>
+ 
+ $("#truk_serial_number").on('input', function(){
+    
+    var truk_serial_number = $("#truk_serial_number").val();
+    
+    $.get("safety_manual_check.php",
+    
+    {
+        serialnumber:truk_serial_number
+        
+    }, function(){
+      console.log(truk_serial_number);   
+    });
+     
+ });
+    
+    
+    
+</script>
