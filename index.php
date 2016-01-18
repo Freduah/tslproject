a714a1ed38b1d3bbb7174680d06390e37cd41672
Administrator <Administrator@FAMPONSAH.tsl.local>
2016-01-18 10:13:22 +0000

    Commit based on recent changes

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <div id="content">		
         <div id="banner-secondary" class="row">
                 <div class="downloads-box four columns push-eight">
-                    <p>Text</p>
+                    <p></p>
                 </div>
                 <div class="features-box row eight columns pull-four">
                  
