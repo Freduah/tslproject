a2d4481e706e44d35215380502c8e10fb1e2d1be
Administrator <Administrator@FAMPONSAH.tsl.local>
2016-01-23 21:46:11 +0000

    Commit based on recent changes

--- tom_data_entry.php
+++ tom_data_entry.php
@@ -1,12 +1,13 @@
+<?php session_start(); ?>
 <?php require_once $_SERVER["DOCUMENT_ROOT"] . 'tslpd/inc/tslpd_header.php'; ?>
-<?php require_once $_SERVER["DOCUMENT_ROOT"] . 'tslpd/nav/safety_check_nav.php'; ?>
+<?php require_once $_SERVER["DOCUMENT_ROOT"] . 'tslpd/db/con_db.php'; ?>
 
 <div id="content-wrapper" class="clearfix row">
   <div class="content-full full-width twelve columns">
     <div id="content">	
-        
-        
-        
+        <table>
+            
+        </table>       
     </div>
   </div>
 </div>    
