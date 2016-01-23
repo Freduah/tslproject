cdb653f33d33cd89c86232c1fea1cac8a1bb2fee
Administrator <Administrator@FAMPONSAH.tsl.local>
2016-01-23 22:08:59 +0000

    Commit based on recent changes

--- tom_data_entry.php
+++ tom_data_entry.php
@@ -6,7 +6,16 @@
   <div class="content-full full-width twelve columns">
     <div id="content">	
         <table>
-            
+            <tr>
+               <td><input type="button" id="country_entry_button" class="styled-login-menu-button" value="COUNTRY ENTRY" /></td>
+               <td><input type="button" id="bdc_entry_button" class="styled-login-menu-button" value="BDC ENTRY" /></td>
+               <td><input type="button" id="depot_entry_button" class="styled-login-menu-button" value="DEPOT ENTRY" /></td>
+            </tr>
+            <tr>
+               <td><input type="button" id="omc_entry_button" class="styled-login-menu-button" value="OMC TYPE ENTRY" /></td>
+               <td><input type="button" id="transporter_entry_button" class="styled-login-menu-button" value="TRANSPORTER ENTRY" /></td>
+               <td><input type="button" id="product_entry_button" class="styled-login-menu-button" value="PRODUCT ENTRY" /></td> 
+            </tr>
         </table>       
     </div>
   </div>
