9cef3ea807e5e0b57ae186fa5440d058fb165325
Administrator <Administrator@FAMPONSAH.tsl.local>
2016-01-25 11:45:27 +0000

    Commit based on recent changes

--- nav/incoming_truck_display_nav.php
+++ nav/incoming_truck_display_nav.php
@@ -5,13 +5,4 @@
         <p style="color: white; font-size: x-large;">Incoming Trucks</p>
     </ul>
 </div>
-
-<form method="get" class="searchform" action="#">
-    <button type="submit" class="icon-search"><span class="visuallyhidden">search</span></button>
-    <label>
-        <span class="visuallyhidden">Search jQuery</span>
-        <input type="text" name="s" value=""
-                placeholder="Search">
-    </label>
-</form>
 </nav>
