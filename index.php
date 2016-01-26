f3ceb3da4756b56c1c78698ff8220f27bd70177d
Administrator <Administrator@FAMPONSAH.tsl.local>
2016-01-26 10:40:47 +0000

    Commit based on recent changes

--- index.php
+++ index.php
@@ -18,13 +18,13 @@
                     <div class="form-group">
                       <label for="inputEmail" class="col-sm-2 control-label">Email</label>
                       <div class="col-sm-10">
-                          <input type="email" class="form-control" id="user-login-email" name="user-login-email" placeholder="Email">
+                          <input type="email" class="text ui-corner-all ui-widget-content" id="user-login-email" name="user-login-email" placeholder="Email">
                       </div>
                     </div>
                     <div class="form-group">
                       <label for="inputPassword" class="col-sm-2 control-label">Password</label>
                       <div class="col-sm-10">
-                          <input type="password" class="form-control" id="user-login-pass" name="user-login-pass" placeholder="Password">
+                          <input type="password" class="text ui-corner-all ui-widget-content" id="user-login-pass" name="user-login-pass" placeholder="Password">
                       </div>
                     </div>
 
